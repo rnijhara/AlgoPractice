a4a55543e194fb1bf3f023953b5092a8a4b64e94
rex <rex@rex-Inspiron-5421>
2016-07-09 00:29:48 +0530

    GCD

---
+++ AlgoPractice/src/random/GCD.java
@@ -0,0 +1,28 @@
+package random;
+
+/**
+ *
+ * @author rex
+ */
+public class GCD {
+    public static void main(String[] args) {
+        System.out.println(gcdIterative(5, 10));
+        System.out.println(gcdRecursive(5, 10));
+    }
+    
+    static int gcdRecursive(int a, int b) {
+        if(a == 0)
+            return b;
+        return gcdRecursive(b%a, a);
+    }
+    
+    static int gcdIterative(int a, int b) {
+        int t;
+        while(a != 0) {
+            t = a;
+            a = b%a;
+            b = t;
+        }
+        return b;
+    }
+}
